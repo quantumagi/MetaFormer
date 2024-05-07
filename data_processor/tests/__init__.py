a228b6e582707682d191fd00861298d409625f52
quantumagi <someguy.fromafrica@gmail.com>
2024-05-07 23:46:02 +1000

    Create test user

--- data_processor/tests/__init__.py
+++ data_processor/tests/__init__.py
@@ -1,6 +1,11 @@
 import os
 import django
+from django.contrib.auth.models import User
 
-"""Run administrative tasks."""
+# Run administrative tasks.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'metaformer.settings')
 django.setup()
+
+# Add a test user.
+if not User.objects.filter(username='test').exists():
+    User.objects.create_user('test', 'test@test.com', 'testpassword')
